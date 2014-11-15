bec20b137542f36c927a45dd1a5ebc4a9bf78b92
mell9413 <mell9413@hotmail.com>
2014-11-15 15:04:08 -0600

    implementacion de la clase grafo con generics

---
+++ FaceChef/src/main/java/Nodo.java
@@ -0,0 +1,22 @@
+public class Nodo <E>{
+        
+    private E dato;
+    public Lista <Nodo <E>> lista;
+
+    public Nodo (E dato){
+        this.dato = dato;
+        this.lista = new Lista <Nodo <E>> ();
+    }
+    
+    public E obtenerDato(){
+        return dato;
+    }
+
+    public void agregarRelacion (Nodo n){
+        this.lista.agregar (n);
+    }
+
+    public void eliminarRelacion (Nodo n){
+       this.lista.eliminar(n);
+    }
+}
