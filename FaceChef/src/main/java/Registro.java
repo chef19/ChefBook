b8349481dab93b130b339d6f6f677673cb13b334
mell9413 <mell9413@hotmail.com>
2014-11-20 12:23:01 -0600

    registro

--- FaceChef/src/main/java/Registro.java
+++ FaceChef/src/main/java/Registro.java
@@ -6,37 +6,67 @@ import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.validation.constraints.NotNull;
 
-@Named (value="hola")
+@Named (value="Registro")
 @RequestScoped
 public class Registro {
     @NotNull
     private String nombre;
     @NotNull
+    private String carrera;
+    @NotNull
+    private String fecha;
+    @NotNull
+    private String correo;
+    @NotNull
     private String contraseña;
     @NotNull
     private String carnet;
-    
-    public String getB() {
+
+    public String getNombre() {
         return nombre;
     }
 
-    public void setB(String nombre) {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public String getC() {
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
         return contraseña;
     }
 
-    public void setC(String contraseña) {
+    public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     }
-    
-    public String getD() {
+
+    public String getCarnet() {
         return carnet;
     }
 
-    public void setD(String carnet) {
+    public void setCarnet(String carnet) {
         this.carnet = carnet;
     }
     
@@ -44,7 +74,6 @@ public class Registro {
         if("".equals(nombre) || "".equals(contraseña) || "".equals(carnet)){
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Información necesaria faltante"));
     }
-        
     else{
         ArrayList list = new ArrayList();
         list.add(this.nombre);
